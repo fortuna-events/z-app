5b82b570637b036576da241b64e8b77f69978ef0
clement-gouin <gouin.cle@gmail.com>
2025-02-28 09:14:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Z-App
+*Working template for URL encoded app*
+
+<!-- TODO: 1. rename app -->
+
+### [Tool link](https://clement-gouin.github.io/z-app/)
+
+## Data format
+
+<!-- TODO: 3. change data format -->
+
+Format is made line by line
+
+Header (1 line):
+```txt
+1   Title and description (html, <h1> on plain text)
+```
+
+After that each link is defined as follows:
+```txt
+1   Data (html)
+```
+
+## Samples
+
+<!-- TODO: 4. change sample -->
+
+```txt
+Url encoded app template
+<i>Italic text</i>
+<b>Bold text</b>
+<pre>code text</pre>
+<a href="https://google.com">link</a>
+```
+
+## Use this template
+
+<!-- TODO: 6. remove this part -->
+
+> Every task is indicated with a TODO
+
+1. [ ] Rename app in [README.md](./README.md), [main.js](./main.js) and [index.html](./index.html)
+2. [ ] Change app hue and saturation in [style.css](./style.css)
+3. [ ] Change data format in [README.md](./README.md) and [main.js](./main.js)
+4. [ ] Change sample in [README.md](./README.md) and [main.js](./main.js)
+5. [ ] Implement your custom logic in [main.js](./main.js) and [index.html](./index.html)
+6. [ ] Remove this part and all TODO
+
+## Apps using this template
+
+* [Treasure-Finder](https://github.com/clement-gouin/treasure-finder)
+* [On-The-Quizz](https://github.com/clement-gouin/on-the-quizz)
+* [Cross-Roads](https://github.com/clement-gouin/on-the-quizz)
+* [Dice-Roller](https://github.com/clement-gouin/cross-roads)
+
+## Tips
+
+* [Material design colors](https://materialui.co/colors/) are available, you can use `class="red-500"` on your HTML
+* [Lucide icons](https://lucide.dev/icons) are available, you can use `<i icon=house></i>` on your HTML
---
+++ main.js
@@ -0,0 +1,158 @@
+/* exported app */
+
+// TODO: 3. change data format
+
+const HELP_HEADER = ["Title and description (html, <h1> on plain text)"];
+const HELP_PART = ["Data (html)"];
+
+const utils = {
+  base64URLTobase64(str) {
+    const base64Encoded = str.replace(/-/g, "+").replace(/_/g, "/");
+    const padding =
+      str.length % 4 === 0 ? "" : "=".repeat(4 - (str.length % 4));
+    return base64Encoded + padding;
+  },
+  base64tobase64URL(str) {
+    return str.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+  },
+  decodeData(str) {
+    return LZString.decompressFromBase64(
+      utils.base64URLTobase64(str.split("").reverse().join(""))
+    );
+  },
+  encodeData(str) {
+    return utils
+      .base64tobase64URL(LZString.compressToBase64(str))
+      .split("")
+      .reverse()
+      .join("");
+  },
+  normalize(str) {
+    return str
+      .trim()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase();
+  },
+};
+
+let app = {
+  data() {
+    return {
+      debug: true,
+      // TODO: 4. change sample
+      debugData:
+        "Url encoded app template\n<i>Italic text</i>\n<b>Bold text</b>\n<pre>code text</pre>\n<a href='https://google.com'>link</a>",
+      debugUrl: "",
+      editor: {
+        numbersCols: 0,
+        numbersText: "",
+        overlayText: "",
+      },
+      parsed: {
+        // TODO: 5. implement custom logic
+        header: "",
+        data: [],
+      },
+    };
+  },
+  computed: {},
+  watch: {
+    debugData(value) {
+      this.readZData(value);
+      this.updateEditor(value);
+      this.updateDebugUrl(value);
+    },
+  },
+  methods: {
+    showApp() {
+      document.getElementById("app").setAttribute("style", "");
+    },
+    updateIcons() {
+      lucide.createIcons({
+        nameAttr: "icon",
+        attrs: {
+          width: "1.1em",
+          height: "1.1em",
+        },
+      });
+    },
+    initApp() {
+      const url = new URL(window.location);
+      if (url.searchParams.get("z") !== null) {
+        this.debug = this.readZData(
+          utils.decodeData(url.searchParams.get("z"))
+        );
+      }
+      if (this.debug) {
+        this.readZData(this.debugData);
+        this.updateEditor(this.debugData);
+        this.updateDebugUrl(this.debugData);
+      }
+    },
+    updateDebugUrl(value) {
+      this.debugUrl = value.trim().length
+        ? window.location.pathname + "?z=" + utils.encodeData(value.trim())
+        : "";
+    },
+    updateEditor(value) {
+      const debugDataSplit = value.split("\n");
+      let size = HELP_HEADER.length + HELP_PART.length;
+      while (debugDataSplit.length > size) {
+        size += HELP_PART.length;
+      }
+      const lines = Array(size).fill(0);
+      this.editor.numbersText = lines.map((v, i) => `${i + 1}.`).join("\n");
+      this.editor.overlayText = lines
+        .map((v, i) => {
+          if (debugDataSplit.length > i && debugDataSplit[i].trim().length) {
+            return " ".repeat(debugDataSplit[i].length);
+          }
+          if (HELP_HEADER.length > i) {
+            return HELP_HEADER[i];
+          }
+          return HELP_PART[(i - HELP_HEADER.length) % HELP_PART.length];
+        })
+        .join("\n");
+      this.editor.numbersCols = lines.length.toString().length + 1;
+    },
+    readZData(str) {
+      // TODO: 5. implement custom logic
+      this.debugData = str;
+      const parts = str.split("\n");
+      if (parts.length < 1) {
+        return true;
+      }
+      this.parsed.header = parts.shift();
+      if (!/<[^>]*>/.test(this.parsed.header)) {
+        this.parsed.header = `<h1>${this.parsed.header}</h1>`;
+      }
+      this.parsed.data = [];
+      while (parts.length) {
+        this.parsed.data.push(parts.shift());
+      }
+      return false;
+    },
+  },
+  beforeMount: function () {
+    this.initApp();
+  },
+  mounted: function () {
+    console.log("app mounted");
+    setTimeout(this.showApp);
+    this.updateIcons();
+    this.$refs.code?.addEventListener("scroll", () => {
+      this.$refs.numbers.scrollTop = this.$refs.code.scrollTop;
+      this.$refs.overlay.scrollTop = this.$refs.code.scrollTop;
+      this.$refs.overlay.scrollLeft = this.$refs.code.scrollLeft;
+    });
+  },
+  updated: function () {
+    this.updateIcons();
+  },
+};
+
+window.onload = () => {
+  app = Vue.createApp(app);
+  app.mount("#app");
+};
