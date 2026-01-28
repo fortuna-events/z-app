a961e13b2ee16e91f9f7a3293a5847fed667fc1d
clement-gouin <gouin.cle@gmail.com>
2026-01-28 21:55:32 +0100

    feat: add cookies lib

---
+++ src/lib/cookies.ts
@@ -0,0 +1,18 @@
+export function setCookie(name: string, value: string, days = 30) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${name}=${value}; path=/; ${expires}`;
+}
+
+export function getCookie(name: string, defaultValue: string): string {
+    const prefix = `${name}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookies = decodedCookie.split(";");
+    for (const cookie of cookies) {
+      if (cookie.trim().startsWith(prefix)) {
+        return cookie.trim().substring(prefix.length, cookie.length);
+      }
+    }
+    return defaultValue;
+}
